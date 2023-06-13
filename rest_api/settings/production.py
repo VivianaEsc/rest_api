2d02c6a9224a601eea939a6ec29f1353cbe2494b
Henry <Administrador@RFR.LOCAL>
2023-06-13 12:34:20 -0500

    PRoduccion Ip

--- rest_api/settings/production.py
+++ rest_api/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.229.42.115']
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
