49ce1e7e246848e2610f4585d427015eb9336637
Henry <Administrador@RFR.LOCAL>
2023-06-20 10:04:55 -0500

    Cors cambiado a local

--- rest_api/settings/base.py
+++ rest_api/settings/base.py
@@ -108,32 +108,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
-
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
-
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = [
-    'http://192.168.0.117:8080',
-    'http://localhost:8080',
-    'http://localhost:4200'
-]
 
--- rest_api/settings/local.py
+++ rest_api/settings/local.py
@@ -20,4 +20,32 @@ DATABASES = {
 
 STATIC_URL = 'static/'
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    'http://192.168.0.117:8080',
+    'http://localhost:8080',
+    'http://localhost:4200'
+]
 
